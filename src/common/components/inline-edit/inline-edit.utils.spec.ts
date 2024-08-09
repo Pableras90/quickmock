10b40859f96238350f6d1f326525fd815d3802d8
Pablo <p.reinaldo90@gmail.com>
2024-08-09 18:17:03 +0200

    update inline-edit tests

--- src/common/components/inline-edit/inline-edit.utils.spec.ts
+++ src/common/components/inline-edit/inline-edit.utils.spec.ts
@@ -1,34 +1,66 @@
 import { addPxSuffix, calculateCoordinateValue } from "./inline-edit.utils";
 
 describe('addPxSuffix', () => {
-    test("should return 'px' with a postiive number", () => {
-        expect(addPxSuffix(10)).toBe('10px');
+    it("should return 'px' with a positive number", () => {
+        const value: number = 10;
+        const result = addPxSuffix(value);
+
+        expect(result).toBe('10px');
     })
-    test("should return 'px' with a negative number", () => {
-        expect(addPxSuffix(-10)).toBe('-10px');
+    it("should return 'px' with a negative number", () => {
+        const value: number = -10;
+        const result = addPxSuffix(value);
+
+        expect(result).toBe('-10px');
     })
-    test("should return 'px' with a zero", () => {
-        expect(addPxSuffix(0)).toBe('0px');
+    it("should return 'px' with a zero", () => {
+        const value: number = 0;
+        const result = addPxSuffix(value);
+
+        expect(result).toBe('0px');
     })
-    test("should return 'px' with a decimal number", () => {
-        expect(addPxSuffix(0.1)).toBe('0.1px');
+    it("should return 'px' with a decimal number", () => {
+        const value: number = 0.1;
+        const result = addPxSuffix(value);
+
+        expect(result).toBe('0.1px');
     })
 });
 
 describe('calculateCoordinateValue', () => {
-    test("should return the coordinate value multiplied by scale with 'px' suffix", () => {
-        expect(calculateCoordinateValue(5, 2)).toBe('10px');
+    it("should return the coordinate value multiplied by scale with 'px' suffix", () => {
+        const value: number = 5;
+        const scale: number = 2;
+        const result = calculateCoordinateValue(value, scale);
+
+        expect(result).toBe('10px');
     })
-    test("should handle negative values correctly", () => {
-        expect(calculateCoordinateValue(-5, 2)).toBe('-10px');
+    it("should handle negative values correctly", () => {
+        const value: number = -5;
+        const scale: number = 2;
+        const result = calculateCoordinateValue(value, scale);
+
+        expect(result).toBe('-10px');
     })
-    test("should handle negative scales correctly", () => {
-        expect(calculateCoordinateValue(5, -2)).toBe('-10px');
+    it("should handle negative scales correctly", () => {
+        const value: number = 5;
+        const scale: number = -2;
+        const result = calculateCoordinateValue(value, scale);
+
+        expect(result).toBe('-10px');
     })
-    test("should return '0px' when value is '0'", () => {
-        expect(calculateCoordinateValue(0, 2)).toBe('0px');
+    it("should return '0px' when value is '0'", () => {
+        const value: number = 0;
+        const scale: number = 2;
+        const result = calculateCoordinateValue(value, scale);
+
+        expect(result).toBe('0px');
     })
-    test("should return correctly when scale is decimal", () => {
-        expect(calculateCoordinateValue(1, 1.5)).toBe('1.5px');
+    it("should return correctly when scale is decimal", () => {
+        const value: number = 1;
+        const scale: number = 1.5;
+        const result = calculateCoordinateValue(value, scale);
+
+        expect(result).toBe('1.5px');
     })
 });

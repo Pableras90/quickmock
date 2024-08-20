56cf6fea366af0a1f99e6f6e6189a47cc2734bab
Braulio <braulio.diez@gmail.com>
2024-08-20 18:42:17 +0200

    solucion

--- src/pods/toolbar/components/save-button/save-button.tsx
+++ src/pods/toolbar/components/save-button/save-button.tsx
@@ -2,28 +2,55 @@ import { SaveIcon } from '@/common/components/icons/save-icon.component';
 import classes from '@/pods/toolbar/toolbar.pod.module.css';
 import { ToolbarButton } from '../toolbar-button';
 import { useCanvasContext } from '@/core/providers';
+import { saveFileModern } from '@/common/export';
 
-const DEFAULT_FILE_NAME = 'document';
-
+const DEFAULT_FILE_NAME = 'mymockui';
+const DEFAULT_FILE_EXTENSION = 'qm';
+const DEFAULT_EXTENSION_DESCRIPTION = 'QuicMock files';
 
 export const SaveButton: React.FC = () => {
-  const { fileName, setFileName } = useCanvasContext();
-  const saveFile = (openedFileName: string) => {
-    const newfileName = openedFileName !== '' ? openedFileName : DEFAULT_FILE_NAME;
-    //const url = URL.createObjectURL(blob);
-    //const blob = new Blob([jsonString], { type: 'application/json' });
-    /*console.log('Save');
-    const myObject = { name: 'hello' };
-    const jsonString = JSON.stringify(myObject);
+  //const { fileName, setFileName } = useCanvasContext();
+
+  const { shapes } = useCanvasContext();
+
+  const serializeShapes = (): string => {
+    return JSON.stringify(shapes);
+  };
+
+  const OldBrowsersDownloadFile = (filename: string) => {
+    const blob = new Blob([serializeShapes()], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
     const a = document.createElement('a');
     a.href = url;
-    a.download = `${fileName}.json`;
+    a.download = `${filename}.qm`;
     a.click();
-    URL.revokeObjectURL(url);*/
+    URL.revokeObjectURL(url);
+  };
+
+  const newBrowsersDownloadFile = async (filename: string) => {
+    const content = serializeShapes();
+
+    const savedFilename = await saveFileModern(
+      {
+        filename,
+        extension: DEFAULT_FILE_EXTENSION,
+        description: DEFAULT_EXTENSION_DESCRIPTION,
+      },
+      content
+    );
+
+    console.log('savedFilename', savedFilename);
+  };
+
+  const saveFile = () => {
+    const filename = DEFAULT_FILE_NAME;
+
     if (window.showDirectoryPicker === undefined) {
-      console.log("download file")
+      OldBrowsersDownloadFile(filename);
     } else {
-      console.log("save file")
+      // Implementalo para Chrome con lo moderno
+      newBrowsersDownloadFile(filename);
     }
   };
 
@@ -36,4 +63,3 @@ export const SaveButton: React.FC = () => {
     />
   );
 };
-
